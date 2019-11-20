3df56599d01c02d3edb110777ec894571b49a35a
javierescamilla <javier_escamillaj@hotmail.com>
2019-11-20 02:35:32 -0600

    Testing 2

---
+++ public/home_tab/script.js
@@ -0,0 +1,22 @@
+function showFoundDogs(){
+	fetch('/blog-posts')
+		.then( response => {
+
+			if ( response.ok ){
+                return response.json();
+			}
+			throw new Error ( response.statusText );
+		})
+		.then( responseJSON => {          
+			for ( let i = 0; i < responseJSON.length; i ++ ){
+                $('.Found_dogs_section').find('ul')
+                    .append(`<b>${responseJSON[i].title}</b>`)
+			}
+		})
+		.catch( err => {
+            console.log("Internal error")
+			console.log( err );
+		});
+}
+
+showFoundDogs()
