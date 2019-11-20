1466c7d88ad90a2d531f8a42a7adf63d97c527fd
javierescamilla <javier_escamillaj@hotmail.com>
2019-11-20 02:26:10 -0600

    Testing 1

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/javier-blog-post";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/find-my-dog-blog-post";
 exports.PORT = process.env.PORT || 8080;
