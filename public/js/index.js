4462abe9a55563987d7e0629efc49a6659ef121f
javierescamilla <javier_escamillaj@hotmail.com>
2019-11-21 01:37:14 -0600

    Testing Christian

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,9 @@
-function showFoundDogs(){
-	fetch('/found-dogs')
-		.then( response => {
-
-			if ( response.ok ){
-                return response.json();
-			}
-			throw new Error ( response.statusText );
-		})
-		.then( responseJSON => {   
-			console.log(responseJSON)       
-			for ( let i = 0; i < responseJSON.length; i ++ ){
-				$('.Found_dogs_section').find('ul')
-                    .append(`<b>${responseJSON[i].breed}</b>
-                             <b>${responseJSON[i].color}</b>
-                             <b>${responseJSON[i].date}</b>
-                             <b>${responseJSON[i].comments}</b>
-                             <b>${responseJSON[i].address}</b>`)
-			}
-		})
-		.catch( err => {
-            console.log("Internal error")
-			console.log( err );
-		});
-}
+var indexLostDog = 0;
+var indexFoundDog = 0;
+var indexSeenDog = 0;
+var lengthLostDog;
+var lengthFoundDog;
+var lengthSeenDog;
 
 function showLostDogs(){
 	fetch('/lost-dogs')
@@ -33,16 +14,52 @@ function showLostDogs(){
 			}
 			throw new Error ( response.statusText );
 		})
-		.then( responseJSON => {   
-			console.log(responseJSON)       
-			for ( let i = 0; i < responseJSON.length; i ++ ){
-				$('.Lost_dog_section').find('ul')
-                    .append(`<b>${responseJSON[i].name}</b>
-                             <b>${responseJSON[i].breed}</b>
-                             <b>${responseJSON[i].color}</b>
-                             <b>${responseJSON[i].date}</b>
-                             <b>${responseJSON[i].reward}</b>`)
+		.then( responseJSON => {
+            //lengthLostDog = responseJSON.length;
+            $('#imagePreviewLost>.imagePreviewPhoto').attr('src','data:image/png;base64,'+ responseJSON[indexFoundDog].image);
+            $('#imagePreviewLost>.imagePreviewText').css('display','none');
+            $('#imagePreviewLost>.imagePreviewPhoto').css('display','block');  
+            $('#nameDogPostFeed').append(`${responseJSON[indexFoundDog].name}`);
+            $('#breedDogPostFeed').append(`${responseJSON[indexFoundDog].breed}`);
+            $('#colorDogPostFeed').append(`${responseJSON[indexFoundDog].color}`);
+            $('#dateDogPostFeed').append(`${responseJSON[indexFoundDog].date}`);
+            $('#rewardDogPostFeed').append(`${responseJSON[indexFoundDog].reward}`);
+            $('#commentsDogPostFeed').append(`${responseJSON[indexFoundDog].comments}`);
+            $('#addrDogPostFeed').append(`${responseJSON[indexFoundDog].address}`);
+            $('#postalCodeDogPostFeed').append(`${responseJSON[indexFoundDog].zipCode}`);
+            $('#cityDogPostFeed').append(`${responseJSON[indexFoundDog].city}`);
+            $('#stateDogPostFeed').append(`${responseJSON[indexFoundDog].state}`);
+            $('#countryDogPostFeed').append(`${responseJSON[indexFoundDog].country}`);
+		})
+		.catch( err => {
+            console.log("Internal error")
+			console.log( err );
+		});
+}
+
+function showFoundDogs(){
+	fetch('/found-dogs')
+		.then( response => {
+
+			if ( response.ok ){
+                return response.json();
 			}
+			throw new Error ( response.statusText );
+		})
+		.then( responseJSON => {   
+            //lengthFoundDog = responseJSON.length;
+            $('#imagePreviewFound>.imagePreviewPhoto').attr('src','data:image/png;base64,'+ responseJSON[indexFoundDog].image);
+            $('#imagePreviewFound>.imagePreviewText').css('display','none');
+            $('#imagePreviewFound>.imagePreviewPhoto').css('display','block');
+            $('#breedDogPostFeed').append(`${responseJSON[indexFoundDog].breed}`);
+            $('#colorDogPostFeed').append(`${responseJSON[indexFoundDog].color}`);
+            $('#dateDogPostFeed').append(`${responseJSON[indexFoundDog].date}`);
+            $('#commentsDogPostFeed').append(`${responseJSON[indexFoundDog].comments}`);
+            $('#addrDogPostFeed').append(`${responseJSON[indexFoundDog].address}`);
+            $('#postalCodeDogPostFeed').append(`${responseJSON[indexFoundDog].zipCode}`);
+            $('#cityDogPostFeed').append(`${responseJSON[indexFoundDog].city}`);
+            $('#stateDogPostFeed').append(`${responseJSON[indexFoundDog].state}`);
+            $('#countryDogPostFeed').append(`${responseJSON[indexFoundDog].country}`);
 		})
 		.catch( err => {
             console.log("Internal error")
@@ -60,15 +77,18 @@ function showSeenDogs(){
 			throw new Error ( response.statusText );
 		})
 		.then( responseJSON => {   
-			console.log(responseJSON)       
-			for ( let i = 0; i < responseJSON.length; i ++ ){
-				$('.Seen_dog_section').find('ul')
-                    .append(`<b>${responseJSON[i].name}</b>
-                             <b>${responseJSON[i].breed}</b>
-                             <b>${responseJSON[i].color}</b>
-                             <b>${responseJSON[i].date}</b>
-                             <b>${responseJSON[i].reward}</b>`)
-			}
+            $('#imagePreviewSeen>.imagePreviewPhoto').attr('src','data:image/png;base64,'+ responseJSON[indexFoundDog].image);
+            $('#imagePreviewSeen>.imagePreviewText').css('display','none');
+            $('#imagePreviewSeen>.imagePreviewPhoto').css('display','block');
+            $('#breedDogPostFeed').append(`${responseJSON[indexFoundDog].breed}`);
+            $('#colorDogPostFeed').append(`${responseJSON[indexFoundDog].color}`);
+            $('#dateDogPostFeed').append(`${responseJSON[indexFoundDog].date}`);
+            $('#commentsDogPostFeed').append(`${responseJSON[indexFoundDog].comments}`);
+            $('#addrDogPostFeed').append(`${responseJSON[indexFoundDog].address}`);
+            $('#postalCodeDogPostFeed').append(`${responseJSON[indexFoundDog].zipCode}`);
+            $('#cityDogPostFeed').append(`${responseJSON[indexFoundDog].city}`);
+            $('#stateDogPostFeed').append(`${responseJSON[indexFoundDog].state}`);
+            $('#countryDogPostFeed').append(`${responseJSON[indexFoundDog].country}`);
 		})
 		.catch( err => {
             console.log("Internal error")
@@ -76,6 +96,23 @@ function showSeenDogs(){
 		});
 }
 
-showFoundDogs();
+/*function getNewPosts(){
+    $('#lostDogBack').on('reset', function(event){
+        event.preventDefault();
+        if(indexLostDog > 0){
+            indexLostDog--;
+            showLostDogs();
+        }
+    });
+    $('#lostDogNext').on('submit', function(event){
+        event.preventDefault();
+        if(indexLostDog < ){
+            indexLostDog--;
+            showLostDogs();
+        }
+    });*/
+
+
 showLostDogs();
+showFoundDogs();
 showSeenDogs();
