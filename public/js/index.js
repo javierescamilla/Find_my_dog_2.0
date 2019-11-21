bd33b94ae38af08b2c7ac01709afb6ab4141a53f
javierescamilla <javier_escamillaj@hotmail.com>
2019-11-21 00:54:58 -0600

    Testing GET

--- public/js/index.js
+++ public/js/index.js
@@ -24,4 +24,31 @@ function showFoundDogs(){
 		});
 }
 
+function showLostDogs(){
+	fetch('/lost-dogs')
+		.then( response => {
+
+			if ( response.ok ){
+                return response.json();
+			}
+			throw new Error ( response.statusText );
+		})
+		.then( responseJSON => {   
+			console.log(responseJSON)       
+			for ( let i = 0; i < responseJSON.length; i ++ ){
+				$('.Lost_dog_section').find('ul')
+                    .append(`<b>${responseJSON[i].breed}</b>
+                             <b>${responseJSON[i].picture}</b>
+                             <b>${responseJSON[i].color}</b>
+                             <b>${responseJSON[i].foundOn}</b>
+                             <b>${responseJSON[i].comments}</b>`)
+			}
+		})
+		.catch( err => {
+            console.log("Internal error")
+			console.log( err );
+		});
+}
+
 showFoundDogs();
+showLostDogs();
