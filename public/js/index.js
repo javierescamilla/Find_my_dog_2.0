949d98e794ed2de0e1c46fd57fb39844487a4631
javierescamilla <javier_escamillaj@hotmail.com>
2019-11-21 03:13:13 -0600

    Testing Christian_10

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ var lengthFoundDog;
 var lengthSeenDog;
 
 function showLostDogs(){
-	fetch('/lost-dogs')
+	fetch('/lost-dogs/')
 		.then( response => {
 
 			if ( response.ok ){
@@ -52,7 +52,7 @@ function showLostDogs(){
 }
 
 function showFoundDogs(){
-	fetch('/found-dogs')
+	fetch('/found-dogs/')
 		.then( response => {
 
 			if ( response.ok ){
@@ -96,7 +96,7 @@ function showFoundDogs(){
 }
 
 function showSeenDogs(){
-	fetch('/seen-dogs')
+	fetch('/seen-dogs/')
 		.then( response => {
 
 			if ( response.ok ){
